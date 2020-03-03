29c7aba60f7bde0727e407019e7a51a54d456f08
Hana Harencarova <h.harencarova@gmail.com>
2020-03-03 15:12:24 +0100

    Initial commit

---
+++ old/register.php
@@ -0,0 +1,46 @@
+<?php
+
+if($_POST["submit"]) {
+    // $recipient="hi@momslearntocode.com";
+    $recipient=$_POST["senderEmail"];
+  
+    $subject="Hey, you just registred for the course";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?><!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>HEad title</title>
+</head>
+
+<body>
+
+    <?=$thankYou ?>
+
+    <form method="post" action="register.php">
+        <label>Name:</label>
+        <input name="sender">
+
+        <label>Email address:</label>
+        <input name="senderEmail">
+
+        <label>Message:</label>
+        <textarea rows="5" cols="20" name="message"></textarea>
+
+        <input type="submit" name="submit">
+    </form>
+
+</body>
+
+</html>
